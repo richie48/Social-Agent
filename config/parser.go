d0a1eb059323f0df930eb25d3a2fcffd95003465
Richard <odiorrichard@gmail.com>
2026-01-15 01:42:45 +0000

    make single posting time for now

--- config/parser.go
+++ config/parser.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 )
 
+// Config holds the configuration for the Social Media Agent
 type Config struct {
 	// Credentials
 	TwitterXBearerToken string
@@ -13,31 +14,29 @@ type Config struct {
 	BlueskyDID          string
 	GeminiAPIKey        string
 	// Agent Configuration
-	PostingScheduleHour1 int
-	PostingScheduleHour2 int
-	FollowUsersPerDay    int
-	LikePostsPerDay      int
-	MaxContentAgeDays    int
-	PostContentTheme     string
-	LogLevel             string
+	PostingScheduleHour int
+	FollowUsersPerDay   int
+	LikePostsPerDay     int
+	MaxContentAgeDays   int
+	PostContentTheme    string
+	LogLevel            string
 }
 
 // Load reads configuration from environment variables and returns config struct
 func Load() (*Config, error) {
 	_ = godotenv.Load()
 	cfg := &Config{
-		TwitterXBearerToken:  os.Getenv("TWITTER_X_BEARER_TOKEN"),
-		BlueskyAccessToken:   os.Getenv("BLUESKY_ACCESS_TOKEN"),
-		BlueskyDID:           os.Getenv("BLUESKY_DID"),
-		GeminiAPIKey:         os.Getenv("GEMINI_API_KEY"),
+		TwitterXBearerToken: os.Getenv("TWITTER_X_BEARER_TOKEN"),
+		BlueskyAccessToken:  os.Getenv("BLUESKY_ACCESS_TOKEN"),
+		BlueskyDID:          os.Getenv("BLUESKY_DID"),
+		GeminiAPIKey:        os.Getenv("GEMINI_API_KEY"),
 		// TODO: posting should not be done at fixed hours
-		PostingScheduleHour1: getEnvInt("POSTING_SCHEDULE_HOUR_1", 0),
-		PostingScheduleHour2: getEnvInt("POSTING_SCHEDULE_HOUR_2", 0),
-		FollowUsersPerDay:    getEnvInt("FOLLOW_USERS_PER_DAY", 0),
-		LikePostsPerDay:      getEnvInt("LIKE_POSTS_PER_DAY", 0),
-		MaxContentAgeDays:    getEnvInt("MAX_CONTENT_AGE_DAYS", 0),
-		PostContentTheme:     os.Getenv("POST_CONTENT_THEME"),
-		LogLevel:             os.Getenv("LOG_LEVEL"),
+		PostingScheduleHour: getEnvInt("POSTING_SCHEDULE_HOUR", 0),
+		FollowUsersPerDay:   getEnvInt("FOLLOW_USERS_PER_DAY", 0),
+		LikePostsPerDay:     getEnvInt("LIKE_POSTS_PER_DAY", 0),
+		MaxContentAgeDays:   getEnvInt("MAX_CONTENT_AGE_DAYS", 0),
+		PostContentTheme:    os.Getenv("POST_CONTENT_THEME"),
+		LogLevel:            os.Getenv("LOG_LEVEL"),
 	}
 	return cfg, nil
 }
